da653a1c146a4b3e75ebbd8d625eddd0bb7e9e6e
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-07-08 23:46:18 +0700

    implement stripe checkout form

--- src/app/(customer)/products/[id]/purchase/page.tsx
+++ src/app/(customer)/products/[id]/purchase/page.tsx
@@ -1,6 +1,7 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
+import { CheckoutForm } from "./_components/CheckoutForm";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
@@ -14,15 +15,20 @@ export default async function PurchasePage({
   });
   if (product == null) return notFound();
 
-  const paymentIntent = stripe.paymentIntents.create({
+  const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
     currency: "USD",
     metadata: { productId: product.id },
   });
 
-  if ((await paymentIntent).client_secret == null) {
+  if (paymentIntent.client_secret == null) {
     throw Error("Stripe failed to create payment intent");
   }
 
-  return <h1>hi</h1>;
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 }
