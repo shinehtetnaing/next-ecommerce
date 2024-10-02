1ad5f0b8f6485289f7cd0f10cce5d27c7635e98c
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-10-02 13:49:17 +0700

    setup stripe webhook & resend

--- src/app/(customer)/products/download/expired/page.tsx
+++ src/app/(customer)/products/download/expired/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Expired() {
   return (
     <>
-      <h1>Download link expired</h1>
+      <h1 className="text-4xl mb-4">Download link expired</h1>
       <Button asChild size="lg">
         <Link href="/orders">Get New Link</Link>
       </Button>
